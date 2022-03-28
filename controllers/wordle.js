ef40464c0044e479a7dd9d1f01adf36cfb149f52
geearen <allengonzales018@gmail.com>
2022-03-28 15:32:54 -0700

    adds get word using filesystem

--- controllers/wordle.js
+++ controllers/wordle.js
@@ -1,16 +1,104 @@
+const utils = require("../utils");
 const db = require("../models");
+const fs = require("fs");
 
-const getWordleBank = (req, res) => {
-  // db.Wordle.find({}, (err, foundWordleBank) => {
-  //   if (err) {
-  //     console.log("Error in Wordle Bank", err);
-
-  //     return res.send("Incomplete WordleBank controller function");
-  //   }
-
-  //   // res.status(200).json({ wordle: foundWordle });
-  // });
-  res.send({ message: "hello" });
+const writeFileAsync = (content) => {
+  const data = JSON.stringify(content);
+  fs.writeFile("./data/word.json", data, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
 };
 
-module.exports = { getWordleBank };
+const getWordleBank = (req, res) => {
+  let word = "";
+  const data = JSON.parse(fs.readFileSync("./data/words.json", "utf8"));
+
+  res.send({ message: data });
+};
+
+const getWordleWord = (req, res) => {
+  const keyDate = utils.createKeyDate("word");
+  const objFile = fs.readFileSync("./data/word.json", "utf8");
+
+  if (objFile.length === 0) {
+    const content = {};
+    const data = JSON.parse(fs.readFileSync("./data/words.json", "utf8"));
+    const getRandWord = data[Math.floor(Math.random() * data.length)];
+    content[`${keyDate}`] = getRandWord;
+    writeFileAsync(content);
+    console.log(content, "new");
+  } else if (!(`${keyDate}` in JSON.parse(objFile))) {
+    const content = {};
+    const data = JSON.parse(fs.readFileSync("./data/words.json", "utf8"));
+    const getRandWord = data[Math.floor(Math.random() * data.length)];
+    content[`${keyDate}`] = getRandWord;
+    writeFileAsync(content);
+  }
+
+  const currentWordJSON = fs.readFileSync("./data/word.json", "utf8");
+  const content = JSON.parse(currentWordJSON);
+
+  console.log(content, "wordExist");
+  res.send({ data: content });
+};
+
+module.exports = { getWordleBank, getWordleWord };
+
+// function fileReader(filePath: string, callback: Function) {
+//   fs.readFile(filePath, (err: string, fileData) => {
+//   if (err) {
+//   return callback && callback(err);
+//   }
+//   try {
+//   const object = JSON.parse(fileData);
+//   return callback && callback(null, object);
+//   } catch (err) {
+//   return callback && callback(err);
+//   }
+
+//   function for writing to file
+//   const writeTextToFileAsync = async (content) => {
+//   await fs.writeFile('./data/word.json', content, (err) => {
+//   console.log(content)
+//   if(err) {
+//   console.log(err)
+//   } else {
+//   console.log('Done writing to file')
+
+// app.get("/word", (req: Request, res: Response) => {
+//   fileReader("./data/word.json", (err: string, fileData) => {
+//     const key = getLocalStorageKey("word");
+
+//     const solution = fileData.hasOwnProperty(key);
+
+//     console.log(solution);
+
+//     if (solution === false) {
+//       const newWord =
+//         words[Math.floor(Math.random() * words.length)].toUpperCase();
+
+//       res.send(newWord);
+
+//       const newObj = `{"${key}" : "${newWord}"}`;
+
+//       writeTextToFileAsync(newObj);
+
+//       console.log(newObj);
+
+//       console.log(newWord);
+//     } else {
+//       res.send({ message: fileData[key] });
+
+//       console.log({ message: fileData[key] });
+//     }
+
+//     if (err) {
+//       console.log("Error reading file:", err);
+
+//       return;
+//     }
+//   });
+// });
--- routes/wordle.js
+++ routes/wordle.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("../controllers");
 
-router.get("/", controller.wordle.getWordleBank);
+router.get("/words", controller.wordle.getWordleBank);
+router.get("/word", controller.wordle.getWordleWord);
 
 module.exports = router;
