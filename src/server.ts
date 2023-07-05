6756d43a244a3df65fd80900ce72a6118d12c6bc
geearen <allengonzales018@gmail.com>
2023-07-04 17:54:46 -0700

    adds ejs views

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,24 @@
 import express, { Application } from "express";
 import cors from "cors";
 import wordleRoute from "./routes/wordle";
+import path from "path";
 
 const port = process.env.PORT || 4000;
 const app: Application = express();
 
+const viewsPath = path.join(__dirname, "./views");
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) =>{
+  const context = {error: null};
+  return res.render("home.ejs", context)
+})
 app.use("/api", wordleRoute);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
